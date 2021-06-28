2d22661b654e5e78261813d144dff18ff8d791fd
jmiquilena <josegregorio.miquilenaprado@externos-cl.cencosud.com>
2021-06-28 12:00:09 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: BuildColumn()
+    );
+  }
+}
+
+class BuildColumn extends StatelessWidget {
+  const BuildColumn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Hola mundo'),
+        Text('Hola mundo')
+      ],
+    );
+  }
+}
