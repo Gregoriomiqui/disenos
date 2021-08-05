7df9ebcd52f316d375226fbe996debf31b41d376
jmiquilena <josegregorio.miquilenaprado@externos-cl.cencosud.com>
2021-08-05 09:25:58 -0400

    se agrega IconButton en el bottonNavigationBar

--- lib/widgets/custom_bottom_navigation.dart
+++ lib/widgets/custom_bottom_navigation.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
+class CustomBottomNavigation extends StatefulWidget {
+  CustomBottomNavigation({Key? key}) : super(key: key);
 
-class CustomBottomNavigation extends StatelessWidget {
+  @override
+  _CustomBottomNavigationState createState() => _CustomBottomNavigationState();
+}
+
+class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      showUnselectedLabels: false,
-      // showSelectedLabels: false,
-      selectedItemColor: Colors.pink,
-      backgroundColor: Color.fromRGBO(55, 57, 84, 1),
-      unselectedItemColor: Color.fromRGBO(116, 117, 152, 1),
-      currentIndex: 0,
-      items: [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.calendar_today_outlined),
-        label: 'Calendario'
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.pie_chart_outline_outlined),
-        label: 'Gráfica'
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.supervised_user_circle_outlined),
-        label: 'Usuarios'
-      ),
-    ]);
+        showUnselectedLabels: false,
+        // showSelectedLabels: false,
+        selectedItemColor: Colors.pink,
+        backgroundColor: Color.fromRGBO(55, 57, 84, 1),
+        unselectedItemColor: Color.fromRGBO(116, 117, 152, 1),
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: IconButton(
+                  icon: Icon(Icons.calendar_today_outlined),
+                  onPressed: () {
+                    setState(() => _currentIndex = 0);
+                  }),
+              label: 'Calendario'),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                  icon: Icon(Icons.pie_chart_outline_outlined),
+                  onPressed: () {
+                    setState(() => _currentIndex = 1);
+                  }),
+              label: 'Gráfica'),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                  icon: Icon(Icons.supervised_user_circle_outlined),
+                  onPressed: () {
+                    setState(() => _currentIndex = 2);
+                  }),
+              label: 'Usuarios'),
+        ]);
   }
 }
